ae0e663ae2728b981ad8da5e8ea26e5f342da9b1
TM <anthonyata.ata3@gmail.com>
2025-11-10 11:08:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ CONFIG = {
     "slippage": 1.5,
     "auto_sell_percent": 1500,
     "token_check_delay": 2,
-    "telegram_token": "7391002298:AAGNo_e56F2PPOn1da978F_6UgvYxHDrx8g",
-    "chat_id": "6946411253"
+    "telegram_token": "",
+    "chat_id": ""
 }
